0ba0a095d9da21b3da6e3c0bd701693a7ad10fa0
moo <mu3980@gmail.com>
2022-04-19 18:30:57 +0900

    finished lecture 2(Django 구조 이해하기)

--- moostaurant/greetings/views.py
+++ moostaurant/greetings/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'foods/index.html')
+    return HttpResponse("<h2>Hola! :)</h2>")
