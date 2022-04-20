3b268b8b9f0405ec18eac6e6d52f2707fbbad903
moo <mu3980@gmail.com>
2022-04-20 20:32:41 +0900

    learning about templates

--- moostaurant/moostaurant/urls.py
+++ moostaurant/moostaurant/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('greetings/', include('greetings.urls')),
+    path('foods/', include('foods.urls'))
 ]
