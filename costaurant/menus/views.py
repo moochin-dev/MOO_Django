3e5daba25df4df56edaf72bc1b89c2e24bf58f6c
moo <mu3980@gmail.com>
2022-04-25 14:19:58 +0900

    finished template and view

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, 'menus/index.html')
+
+def detail(request, menu):
+    context = {"menu": menu}
+    return render(request, "menus/detail.html", context = context)
