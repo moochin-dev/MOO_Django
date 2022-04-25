cb44758938c207e47b359c36dfcf41583a43c3d3
moo <mu3980@gmail.com>
2022-04-25 11:28:38 +0900

    43/79

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse("<h1>코스토랑 오픈!</h1>")
+    return render(request, 'menus/index.html')
